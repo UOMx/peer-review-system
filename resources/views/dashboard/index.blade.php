3a02e7af32171c77ba0e7d92135444528d146bc1
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2017-02-26 10:30:35 +0530

    edit the dashboard - student perspective - UI only

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,13 +19,13 @@
             <div class="">
                 <div class="page-title">
                     <div class="title_left">
-                        <h3>Plain Page</h3>
+                        <h3>My Courses</h3>
                     </div>
 
                     <div class="title_right">
                         <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
                             <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search for...">
+                                <input type="text" class="form-control" placeholder="Search for a course...">
                                 <span class="input-group-btn">
                       <button class="btn btn-default" type="button">Go!</button>
                     </span>
@@ -39,27 +39,121 @@
                 <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
                         <div class="x_panel">
-                            <div class="x_title">
-                                <h2>Plain Page</h2>
-                                <ul class="nav navbar-right panel_toolbox">
-                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li><a href="#">Settings 1</a>
-                                            </li>
-                                            <li><a href="#">Settings 2</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                    </li>
-                                </ul>
-                                <div class="clearfix"></div>
-                            </div>
+                            {{--<div class="x_title">--}}
+                                {{--<h2>Plain Page</h2>--}}
+                                {{--<ul class="nav navbar-right panel_toolbox">--}}
+                                    {{--<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>--}}
+                                    {{--</li>--}}
+                                    {{--<li class="dropdown">--}}
+                                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>--}}
+                                        {{--<ul class="dropdown-menu" role="menu">--}}
+                                            {{--<li><a href="#">Settings 1</a>--}}
+                                            {{--</li>--}}
+                                            {{--<li><a href="#">Settings 2</a>--}}
+                                            {{--</li>--}}
+                                        {{--</ul>--}}
+                                    {{--</li>--}}
+                                    {{--<li><a class="close-link"><i class="fa fa-close"></i></a>--}}
+                                    {{--</li>--}}
+                                {{--</ul>--}}
+                                {{--<div class="clearfix"></div>--}}
+                            {{--</div>--}}
                             <div class="x_content">
-                                Add content to the page ...
+                                @for($i=0;$i<2;$i++)
+                                <div class="row">
+
+                                    <div class="col-md-12">
+
+                                        <!-- price element -->
+                                        <div class="col-md-4 col-sm-6 col-xs-12">
+                                            <div class="pricing">
+                                                <div class="title" style="height: 80px;">
+                                                    <h2>Human Computer Interaction</h2>
+                                                </div>
+                                                <div class="x_content">
+                                                    <div class="">
+                                                        <div class="pricing_features" style="min-height: 0px;">
+                                                            <ul class="list-unstyled text-left">
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Starts on <strong>2nd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Ends on <strong>3rd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> <strong>4 </strong>Assignments Available</li>
+
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="pricing_footer">
+                                                        <a href="javascript:void(0);" class="btn btn-success btn-block" role="button">Download <span> now!</span></a>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <!-- price element -->
+                                        <div class="col-md-4 col-sm-6 col-xs-12">
+                                            <div class="pricing ui-ribbon-container">
+                                                <div class="ui-ribbon-wrapper">
+                                                    <div class="ui-ribbon">
+                                                        New
+                                                    </div>
+                                                </div>
+                                                <div class="title" style="height: 80px;">
+                                                    <h2>Human Computer Interaction</h2>
+                                                </div>
+                                                <div class="x_content">
+                                                    <div class="">
+                                                        <div class="pricing_features" style="min-height: 0px;">
+                                                            <ul class="list-unstyled text-left">
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Starts on <strong>2nd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Ends on <strong>3rd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> <strong>4 </strong>Assignments Available</li>
+
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="pricing_footer">
+                                                        <a href="javascript:void(0);" class="btn btn-primary  btn-block" role="button">Download <span> now!</span></a>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- price element -->
+                                        <div class="col-md-4 col-sm-6 col-xs-12">
+                                            <div class="pricing">
+                                                <div class="title" style="height: 80px;">
+                                                    <h2>Human Computer Interaction</h2>
+                                                </div>
+                                                <div class="x_content">
+                                                    <div class="">
+                                                        <div class="pricing_features" style="min-height: 0px;">
+                                                            <ul class="list-unstyled text-left">
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Starts on <strong>2nd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> Ends on <strong>3rd June 2016</strong></li>
+                                                                <li><i class="fa fa-circle-o text-danger"></i> <strong>4 </strong>Assignments Available</li>
+
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="pricing_footer">
+                                                        <a href="javascript:void(0);" class="btn btn-success btn-block" role="button">Download <span> now!</span></a>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+
+                                    </div>
+                                </div>
+                                <br>
+                                    <hr>
+                                    <br>
+                                @endfor
                             </div>
                         </div>
                     </div>
