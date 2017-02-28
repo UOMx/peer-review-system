8a3e6fc9e3c41bca09f0f1ee8dc4460771953660
Saarrah Isthikar <Saarrah Isthikar>
2017-02-28 06:39:05 +0530

    Adding the table with the rubric and the marks

--- resources/views/receive_grades.blade.php
+++ resources/views/receive_grades.blade.php
@@ -33,9 +33,53 @@
                                             booth letterpress, commodo enim craft beer mlkshk aliquip</p>
                                     </div>
                                     <div role="tabpanel" class="tab-pane fade active in" id="tab_content3" aria-labelledby="profile-tab">
-                                        <p>xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui
-                                            photo booth letterpress, commodo enim craft beer mlkshk </p>
-                                    </div>
+                                        {{--<p>xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui--}}
+                                            {{--photo booth letterpress, commodo enim craft beer mlkshk </p>--}}
+                                        <table class="table table-striped jambo_table bulk_action">
+                                            <thead>
+                                            <tr class="headings">
+
+                                                <th class="column-title">Description </th>
+                                                <th class="column-title">Marks </th>
+
+                                                <th class="bulk-actions" colspan="7">
+                                                    <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+                                                </th>
+                                            </tr>
+                                            </thead>
+
+                                            <tbody>
+                                            <tr class="even pointer">
+
+                                                <td class=" ">Content</td>
+                                                <td class=" ">23</td>
+
+                                            </tr>
+                                            <tr class="odd pointer">
+
+                                                <td class=" ">Organization</td>
+                                                <td class=" ">25</td>
+
+                                            </tr>
+                                            <tr class="even pointer">
+
+                                                <td class=" ">Relativity</td>
+                                                <td class=" ">20</td>
+
+                                            </tr>
+
+
+                                            <tr class="odd pointer">
+
+                                                <td class=" ">Total</td>
+                                                <td class=" ">68</td>
+
+                                            </tr>
+
+
+
+                                            </tbody>
+                                        </table>                                    </div>
                                 </div>
                             </div>
 
