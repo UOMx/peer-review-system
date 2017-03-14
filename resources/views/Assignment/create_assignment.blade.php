12260498788c18a56adde056c938ebe50548d0b4
thuva4 <tharma.thuva@gmail.com>
2017-03-14 14:32:22 +0530

    create rubric

--- resources/views/Assignment/create_assignment.blade.php
+++ resources/views/Assignment/create_assignment.blade.php
@@ -93,7 +93,7 @@
                             <span class="step_no">3</span>
                             <span class="step_descr">
                                               Step 3<br />
-                                              <small>Questions</small>
+                                              <small>Rubric</small>
                                           </span>
                           </a>
                         </li>
@@ -253,12 +253,60 @@
             </div>
             </div>
             </div>
-                      </div>
+          </div>
                       <div id="step-3">
-                        <h2 class="StepTitle">Add Questions</h2>
-                        <p>
-                            Under Construction
-                        </p>
+                        <h2 class="StepTitle">Add Rubric</h2>
+                        <div class="container  col-md-3">
+                            <div class="row">
+                                  <div class="control-group " id="fields">
+                                      <label class="control-label" for="field1">Nice Multiple Form Fields</label>
+                                      <div class="controls"> 
+                                          <form role="form" autocomplete="off">
+                                              <div class="entry input-group">
+                                                  <input class="form-control" name="fields[]" type="text" placeholder="Type something" />
+                                                <span class="input-group-btn">
+                                                      <button class="btn btn-success btn-add " id="fixheight" type="button">
+                                                          <span class="glyphicon glyphicon-plus"></span>
+                                                      </button>
+                                                  </span>
+                                              </div>
+                                          </form>
+                                      <br>
+                                      <small>Press <span class="glyphicon glyphicon-plus gs"></span> to add another form field :)</small>
+                                      </div>
+                                  </div>
+                            </div>
+                          </div>
+                          <script type="text/javascript">
+                                                                $(function()
+                                    {
+                                        $(document).on('click', '.btn-add', function(e)
+                                        {
+                                            e.preventDefault();
+
+                                            var controlForm = $('.controls form:first'),
+                                                currentEntry = $(this).parents('.entry:first'),
+                                                newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+                                            newEntry.find('input').val('');
+                                            controlForm.find('.entry:not(:last) .btn-add')
+                                                .removeClass('btn-add').addClass('btn-remove')
+                                                .removeClass('btn-success').addClass('btn-danger')
+                                                .html('<span class="glyphicon glyphicon-minus"></span>');
+                                        }).on('click', '.btn-remove', function(e)
+                                        {
+                                        $(this).parents('.entry:first').remove();
+
+                                        e.preventDefault();
+                                        return false;
+  });
+});
+                                      $(document).ready(function() {
+        $("#fixheight").click(function() { $("#wizard"). smartWizard("fixHeight"); });
+    });
+
+                          </script>
+                        
                       </div>
                       <div id="step-4">
                         <!-- <h2 class="StepTitle">Grade Method</h2> -->
