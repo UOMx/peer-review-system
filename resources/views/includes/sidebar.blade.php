33f58259ecf34000ec95ee9c022023f092fc086e
PrabodhaNirmani <prabodhanh@gmail.com>
2017-04-02 16:27:28 +0530

    Changing dashboard

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -24,102 +24,199 @@
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3>General</h3>
+                {{--<h3>General</h3>--}}
                 <ul class="nav side-menu">
                     <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a href="#">Dashboard</a></li>
-                            <li><a href="#">Dashboard2</a></li>
-                            <li><a href="#">Dashboard3</a></li>
+                            <li><a href="#">Human Computer Interaction<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+
+                            </li>
+                            <li><a href="#">Signals and Systems<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+
+                            </li>
+                            <li><a href="#">Intelligent Systems<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Programming languages<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Introduction to Java<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Object Oriented Programming<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+
+                            </li>
+                            <li><a href="#">Software Architecture and Design<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Advanced Networking<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Differential Equations<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Linear Algebra<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Computer Security<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="#">Applied Statistics<span class="fa fa-chevron-down"></span></a>
+                                <ul class="nav child_menu">
+                                    <li><a href="#">Assignment 1</a></li>
+                                    <li><a href="#">Assignment 2</a></li>
+                                    <li><a href="#">Assignment 3</a></li>
+
+                                </ul>
+                            </li>
+
                         </ul>
                     </li>
-                    <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">General Form</a></li>
-                            <li><a href="#">Advanced Components</a></li>
-                            <li><a href="#">Form Validation</a></li>
-                            <li><a href="#">Form Wizard</a></li>
-                            <li><a href="#">Form Upload</a></li>
-                            <li><a href="#">Form Buttons</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">General Elements</a></li>
-                            <li><a href="#">Media Gallery</a></li>
-                            <li><a href="#">Typography</a></li>
-                            <li><a href="#">Icons</a></li>
-                            <li><a href="#">Glyphicons</a></li>
-                            <li><a href="#">Widgets</a></li>
-                            <li><a href="#">Invoice</a></li>
-                            <li><a href="#">Inbox</a></li>
-                            <li><a href="#">Calendar</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Tables</a></li>
-                            <li><a href="#">Table Dynamic</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Chart JS</a></li>
-                            <li><a href="#">Chart JS2</a></li>
-                            <li><a href="#">Moris JS</a></li>
-                            <li><a href="#">ECharts</a></li>
-                            <li><a href="#">Other Charts</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">Fixed Sidebar</a></li>
-                            <li><a href="#">Fixed Footer</a></li>
-                        </ul>
-                    </li>
+                    {{--<li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">General Form</a></li>--}}
+                            {{--<li><a href="#">Advanced Components</a></li>--}}
+                            {{--<li><a href="#">Form Validation</a></li>--}}
+                            {{--<li><a href="#">Form Wizard</a></li>--}}
+                            {{--<li><a href="#">Form Upload</a></li>--}}
+                            {{--<li><a href="#">Form Buttons</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">General Elements</a></li>--}}
+                            {{--<li><a href="#">Media Gallery</a></li>--}}
+                            {{--<li><a href="#">Typography</a></li>--}}
+                            {{--<li><a href="#">Icons</a></li>--}}
+                            {{--<li><a href="#">Glyphicons</a></li>--}}
+                            {{--<li><a href="#">Widgets</a></li>--}}
+                            {{--<li><a href="#">Invoice</a></li>--}}
+                            {{--<li><a href="#">Inbox</a></li>--}}
+                            {{--<li><a href="#">Calendar</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">Tables</a></li>--}}
+                            {{--<li><a href="#">Table Dynamic</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">Chart JS</a></li>--}}
+                            {{--<li><a href="#">Chart JS2</a></li>--}}
+                            {{--<li><a href="#">Moris JS</a></li>--}}
+                            {{--<li><a href="#">ECharts</a></li>--}}
+                            {{--<li><a href="#">Other Charts</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">Fixed Sidebar</a></li>--}}
+                            {{--<li><a href="#">Fixed Footer</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
                 </ul>
             </div>
             <div class="menu_section">
-                <h3>Live On</h3>
-                <ul class="nav side-menu">
-                    <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">E-commerce</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Project Detail</a></li>
-                            <li><a href="#">Contacts</a></li>
-                            <li><a href="#">Profile</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#">403 Error</a></li>
-                            <li><a href="#">404 Error</a></li>
-                            <li><a href="#">500 Error</a></li>
-                            <li><a href="#">Plain Page</a></li>
-                            <li><a href="#">Login Page</a></li>
-                            <li><a href="#">Pricing Tables</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="#level1_1">Level One</a>
-                            <li><a>Level One<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li class="sub_menu"><a href="#">Level Two</a>
-                                    </li>
-                                    <li><a href="#level2_1">Level Two</a>
-                                    </li>
-                                    <li><a href="#level2_2">Level Two</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="#level1_2">Level One</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
-                </ul>
+                {{--<h3>Live On</h3>--}}
+                {{--<ul class="nav side-menu">--}}
+                    {{--<li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">E-commerce</a></li>--}}
+                            {{--<li><a href="#">Projects</a></li>--}}
+                            {{--<li><a href="#">Project Detail</a></li>--}}
+                            {{--<li><a href="#">Contacts</a></li>--}}
+                            {{--<li><a href="#">Profile</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#">403 Error</a></li>--}}
+                            {{--<li><a href="#">404 Error</a></li>--}}
+                            {{--<li><a href="#">500 Error</a></li>--}}
+                            {{--<li><a href="#">Plain Page</a></li>--}}
+                            {{--<li><a href="#">Login Page</a></li>--}}
+                            {{--<li><a href="#">Pricing Tables</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>--}}
+                        {{--<ul class="nav child_menu">--}}
+                            {{--<li><a href="#level1_1">Level One</a>--}}
+                            {{--<li><a>Level One<span class="fa fa-chevron-down"></span></a>--}}
+                                {{--<ul class="nav child_menu">--}}
+                                    {{--<li class="sub_menu"><a href="#">Level Two</a>--}}
+                                    {{--</li>--}}
+                                    {{--<li><a href="#level2_1">Level Two</a>--}}
+                                    {{--</li>--}}
+                                    {{--<li><a href="#level2_2">Level Two</a>--}}
+                                    {{--</li>--}}
+                                {{--</ul>--}}
+                            {{--</li>--}}
+                            {{--<li><a href="#level1_2">Level One</a>--}}
+                            {{--</li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                    {{--<li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>--}}
+                {{--</ul>--}}
             </div>
 
         </div>
